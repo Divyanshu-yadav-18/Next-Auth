f1b15a16da7a6a03f8206ffb39e0bf4729496474
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-05-29 23:49:16 +0530

    password generation

--- src/auth/nextjs/actions.ts
+++ src/auth/nextjs/actions.ts
@@ -3,6 +3,8 @@
 import { z } from "zod"
 import { redirect } from "next/navigation"
 import { signInSchema, signUpSchema } from "./schemas"
+import { generateSalt, hashPassword } from "../core/passwordHasher"
+import { email } from "zod/v4"
 
 export async function signIn(unsafeData: z.infer<typeof signInSchema>) {
   const { success, data } = signInSchema.safeParse(unsafeData)
@@ -17,6 +19,27 @@ export async function signUp(unsafeData: z.infer<typeof signUpSchema>) {
 
   if (!success) return "Unable to create account"
 
+  const existingUser = await db.query.UserTable.findFirst({
+    where: eq(UserTable.email, data.email),
+  })
+
+  if(existingUser != null) return "Account already exist for this email" 
+try{
+  const salt = generateSalt()
+  const hashedPassword = await hashPassword(data.password, salt)
+
+  const[user] = await db
+    .insert(UserTable)
+    .values({
+      name:data.name,
+      email:data.email,
+      hashedPassword,
+      salt
+    }).returning({id: UserTable.id, role : UserTable.role})
+    if(user == null) return "Unable to create an account"
+  }catch{
+    return "Unable to create an account"
+  }
   
   redirect("/")
 }
