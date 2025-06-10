0e3d0a6f4f3a24affb4ff06916f7bb96ee413d9c
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2025-06-10 23:08:56 +0530

    sign in logic

--- src/auth/nextjs/actions.ts
+++ src/auth/nextjs/actions.ts
@@ -3,7 +3,7 @@
 import { z } from "zod"
 import { redirect } from "next/navigation"
 import { signInSchema, signUpSchema } from "./schemas"
-import { generateSalt, hashPassword } from "../core/passwordHasher"
+import { comparePasswords, generateSalt, hashPassword } from "../core/passwordHasher"
 import { UserTable } from "@/drizzle/schema"
 import { db } from "@/drizzle/db"
 import { eq } from "drizzle-orm"
@@ -15,6 +15,24 @@ export async function signIn(unsafeData: z.infer<typeof signInSchema>) {
 
   if (!success) return "Unable to log you in"
 
+  const user = await db.query.UserTable.findFirst({
+    columns: {password: true, salt: true, id:true, email: true, role: true},
+    where: eq(UserTable.email, data.email)
+  })
+
+  if (!user || !user.password || !user.salt) {//will fix
+    return "Unable to log you in";
+  }
+
+  const isCorrectPassward = await comparePasswords({
+    hashedPassword: user.password,
+    password: data.password,
+    salt: user.salt,
+  })
+
+  if(!isCorrectPassward) return "Wrong Password"
+
+  await createUserSession(user, await cookies())
   redirect("/")
 }
 
